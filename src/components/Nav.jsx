4daf2cd3f7ff5283379b033715b24727cd07d751
xandoalka <xandoalka@gmail.com>
2025-05-06 14:04:15 +0700

    changed menu color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,9 +21,9 @@ const Nav = () => {
           <p className="xl:ml-2 lg:text-base text-sm font-light tracking-wide uppercase font-montserrat"><span className="hidden xl:inline">/</span> Freelance Digital Worker</p>
         </span>
         <button onClick={handleButtonClick} className="flex z-10 lg:hidden flex-col justify-center w-10 cursor-pointer">
-          <div className={`bg-secondary rounded-lg h-1 mb-1.5 transition-all duration-300 ${isChecked ? 'w-1/2 transform rotate-45 translate-x-1 translate-y-1 bg-black' : 'w-1/2'}`}></div>
-          <div className={`bg-secondary rounded-lg h-1 transition-all duration-300 w-full ${isChecked ? 'w-11/12 transform -rotate-45 bg-black' : 'w-full'} `}></div>
-          <div className={`bg-secondary rounded-lg h-1 mt-1.5 transition-all duration-300 ${isChecked ? 'w-1/2 transform rotate-45 translate-x-4 -translate-y-1 bg-black' : 'w-3/4'}`}></div>
+          <div className={`bg-tertiary rounded-lg h-1 mb-1.5 transition-all duration-300 ${isChecked ? 'w-1/2 transform rotate-45 translate-x-1 translate-y-1 bg-black' : 'w-1/2'}`}></div>
+          <div className={`bg-tertiary rounded-lg h-1 transition-all duration-300 w-full ${isChecked ? 'w-11/12 transform -rotate-45 bg-black' : 'w-full'} `}></div>
+          <div className={`bg-tertiary rounded-lg h-1 mt-1.5 transition-all duration-300 ${isChecked ? 'w-1/2 transform rotate-45 translate-x-4 -translate-y-1 bg-black' : 'w-3/4'}`}></div>
         </button>
         <ul className={`lg:flex lg:items-center gap-5 uppercase  text-sm  ${isChecked ? 'bg-white flex absolute flex-col gap-8 items-center justify-center w-full -ml-5 h-screen' : 'hidden'}`}>
           <li><Link to={"/"} className={`font-montserrat transition-all duration-200 ease-linear hover:text-tertiary ${location.pathname === '/' ? ' text-tertiary ' : ''}`} href="#">About Me</Link></li>
